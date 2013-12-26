6a630fcf554d0e441d62d41ec0ab6cfe278577ee
Ciara Burkett <ciaraburkett@gmail.com>
2013-12-26 14:26:49 -0500

    Fix better voting algorithm

--- collections/posts.js
+++ collections/posts.js
@@ -47,16 +47,16 @@ Meteor.methods({
         return postId;
     },
     upvote: function(postId) {
-        var user = Meteor.user();
-        // ensure the user is logged in
-        if (!user) {
-            throw new Meteor.Error(401, "You need to login to upvote.");
-            Posts.update({
-                _id: postId,
-                upvoters: {$ne: user._id}
-            }, {
-                $addToSet: {upvoters: user._id},
-                $inc: {votes: 1}
-            });
-        }
-    }); 
+    var user = Meteor.user();
+    // ensure the user is logged in
+    if (!user)
+      throw new Meteor.Error(401, "You need to login to upvote");
+    Posts.update({
+      _id: postId, 
+      upvoters: {$ne: user._id}
+    }, {
+      $addToSet: {upvoters: user._id},
+      $inc: {votes: 1}
+    });
+  }
+});       
